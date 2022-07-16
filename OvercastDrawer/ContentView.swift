6d80766d18aefe0f87bb6d8cb509b840810043cb
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-07-16 11:24:43 -0400

    Initial Commit

---
+++ OvercastDrawer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OvercastDrawer
+//
+//  Created by Ryan Lintott on 2022-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OvercastDrawer/OvercastDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvercastDrawerApp.swift
+//  OvercastDrawer
+//
+//  Created by Ryan Lintott on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct OvercastDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
